d528d4618256ab3fb56ee706266a8a11caaec12f
Roman Grinyov <w3lifer@gmail.com>
2021-11-11 20:01:00 +0300

    1.0.0

---
+++ README.md
@@ -0,0 +1,26 @@
+# [Character counter]()
+
+- [Installation for developers (for Linux users)](#installation-for-developers-for-linux-users)
+- [Sources](#sources)
+    - [Icons](#icons)
+
+---
+
+![Example](img/screenshots/1280x800/example.png)
+
+---
+
+## Installation for developers (for Linux users)
+
+1. Go to the `chrome://extensions`.
+2. Enable `Developer mode` checkbox.
+3. Click on the `Load unpacked` button.
+4. Select the extension directory.
+
+---
+
+## Sources
+
+### Icons
+
+- [Tape](https://flaticon.com/free-icon/tape_4910329)
--- event-page.js
+++ event-page.js
@@ -4,11 +4,26 @@
 
 chrome.runtime.onMessage.addListener(selectedText => {
   chrome.contextMenus.removeAll(() => {
-    let title = selectedText.trim();
-    title = title.split(/\s+/).length + ' Words - ' + title.length + ' Characters';
     chrome.contextMenus.create({
       id: '1',
-      title: title,
+      title: 'Chars: ' + selectedText.length,
+      contexts: ['selection'],
+    });
+    let numberOfWords = 0;
+    if (selectedText.trim().length) {
+      numberOfWords = selectedText.split(/\s+/g).filter(word => word.length);
+      numberOfWords = numberOfWords.length;
+    }
+    chrome.contextMenus.create({
+      id: '2',
+      title: 'Words: ' + numberOfWords,
+      contexts: ['selection'],
+    });
+    let numberOfSpaces = selectedText.match(/\s/g);
+    numberOfSpaces = numberOfSpaces ? numberOfSpaces.length : 0;
+    chrome.contextMenus.create({
+      id: '3',
+      title: 'Spaces: ' + numberOfSpaces,
       contexts: ['selection'],
     });
   });
