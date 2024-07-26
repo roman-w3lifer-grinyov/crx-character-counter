d92bf5d70cc4f97863aba7ef24035d5151487646
Roman Grinyov <w3lifer@gmail.com>
2024-07-26 23:28:29 +0300

    added comments for the future feature

--- js/content-script.js
+++ js/content-script.js
@@ -6,6 +6,11 @@ chrome.runtime.sendMessage(''); // To reset badge after page reload
 
 document.addEventListener('selectionchange', () => chrome.runtime.sendMessage(document.getSelection().toString()));
 
+// document.addEventListener('visibilitychange', () => {
+//   chrome.runtime.sendMessage('');
+//   window.getSelection().empty();
+// });
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.method === 'getSelection') {
     sendResponse({selectedText: document.getSelection().toString()});
